73ab587d6cd043d39e71de3b015c9b32a5d059a0
brinkqiang <brink.qiang@gmail.com>
2019-10-19 23:38:30 +0800

    add

--- src/dmpath/dmpath.cc
+++ src/dmpath/dmpath.cc
@@ -1,8 +1,52 @@
 
 #include "dmpath.h"
 
-int main(int argc, char *argv[])
-{
+const char* DMAPI DMGetDllPath() {
+#ifdef WIN32
+    static char path[MAX_PATH];
+    static bool first_time = true;
 
-    return 0;
+    if (first_time) {
+        first_time = false;
+        GetModuleFileNameA(GetSelfModuleHandle(), path, sizeof(path));
+        char* p = strrchr(path, '\\');
+        *(p) = '\0';
+    }
+
+    return path;
+#elif __APPLE__
+    static char path[MAX_PATH];
+    static bool first_time = true;
+
+    if (first_time) {
+        uint32_t size = sizeof(path);
+        int nRet = _NSGetExecutablePath(path, &size);
+
+        if (nRet != 0) {
+            return "./";
+        }
+
+        char* p = strrchr(path, '/');
+        *(p) = '\0';
+    }
+
+    return path;
+#else
+    static char path[MAX_PATH];
+    static bool first_time = true;
+
+    if (first_time) {
+        first_time = false;
+        int nRet = readlink("/proc/self/exe", path, MAX_PATH);
+
+        if (nRet < 0 || nRet >= MAX_PATH) {
+            return "./";
+        }
+
+        char* p = strrchr(path, '/');
+        *(p) = '\0';
+    }
+
+    return path;
+#endif
 }
