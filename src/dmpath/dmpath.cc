78b55ff71a768117ee0e7f40d7465c232c479392
brinkqiang <brink.qiang@gmail.com>
2024-12-30 11:29:48 +0800

    add

--- src/dmpath/dmpath.cc
+++ src/dmpath/dmpath.cc
@@ -2,6 +2,22 @@
 #include "dmpath.h"
 
 #ifdef _WIN32
+bool IsRunAsAdmin() {
+	BOOL isAdmin = FALSE;
+	PSID adminGroup = nullptr;
+
+	// 分配并初始化一个SID用于管理员组
+	SID_IDENTIFIER_AUTHORITY NtAuthority = SECURITY_NT_AUTHORITY;
+	if (AllocateAndInitializeSid(&NtAuthority, 2, SECURITY_BUILTIN_DOMAIN_RID, DOMAIN_ALIAS_RID_ADMINS, 0, 0, 0, 0, 0, 0, &adminGroup)) {
+		// 检查当前进程是否在管理员组中
+		if (!CheckTokenMembership(nullptr, adminGroup, &isAdmin)) {
+			isAdmin = FALSE;
+		}
+		FreeSid(adminGroup);
+	}
+
+	return isAdmin == TRUE;
+}
 
 bool AddToPath(const std::string& newPath)
 {
--- tools/dmpath/main.cc
+++ tools/dmpath/main.cc
@@ -3,23 +3,6 @@
 #include "dmutil.h"
 #include "dmflags.h"
 
-bool IsRunAsAdmin() {
-	BOOL isAdmin = FALSE;
-	PSID adminGroup = nullptr;
-
-	// 分配并初始化一个SID用于管理员组
-	SID_IDENTIFIER_AUTHORITY NtAuthority = SECURITY_NT_AUTHORITY;
-	if (AllocateAndInitializeSid(&NtAuthority, 2, SECURITY_BUILTIN_DOMAIN_RID, DOMAIN_ALIAS_RID_ADMINS, 0, 0, 0, 0, 0, 0, &adminGroup)) {
-		// 检查当前进程是否在管理员组中
-		if (!CheckTokenMembership(nullptr, adminGroup, &isAdmin)) {
-			isAdmin = FALSE;
-		}
-		FreeSid(adminGroup);
-	}
-
-	return isAdmin == TRUE;
-}
-
 DEFINE_string(addpath, "", "Path to add to the PATH environment variable");
 DEFINE_string(removepath, "", "Path to remove from the PATH environment variable");
 
@@ -32,11 +15,11 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 #ifdef _WIN32
-
 	if (!IsRunAsAdmin()) {
 		std::cerr << "This program requires administrator privileges. Please run as administrator." << std::endl;
 		return 1;
 	}
+#endif
 
 	// 添加路径
 	if (!FLAGS_addpath.empty()) {
@@ -57,6 +40,6 @@ int main(int argc, char** argv) {
 			std::cerr << "Failed to remove path: " << FLAGS_removepath << std::endl;
 		}
 	}
-#endif
+
 	return 0;
 }
